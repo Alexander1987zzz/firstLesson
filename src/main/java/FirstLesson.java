e577a2f851a857ea6023b01cf4021c9291b802f8
Alexander1987zzz <60446688+Alexander1987zzz@users.noreply.github.com>
2021-06-08 18:15:26 +0300

    Update FirstLesson.java

--- src/main/java/FirstLesson.java
+++ src/main/java/FirstLesson.java
@@ -2,6 +2,6 @@ package main.java;
 
 public class FirstLesson {
     public static void main(String[] args) {
-        System.out.println("Hello Jenkins");
+        System.out.println("Hello Jenkins auto");
     }
 }
